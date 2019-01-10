2437ca09fe082a2b39b107b65ad8ae8a48d6f58d
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2019-01-09 21:25:19 -0500

    more code fights fun

--- codeFights.hs
+++ codeFights.hs
@@ -1,3 +1,5 @@
+import           Data.List
+
 {-
 Given a year, return the century it is in.
 The first century spans from the year 1 up to
@@ -6,6 +8,7 @@ the year 101 up to and including the year 200, etc.
 For y = 1905, the output should be 20;
 For y = 1700, the output should be 17.
 -}
+
 centuryFromYear :: Int -> Int
 centuryFromYear y
   | m == 0    = d
@@ -20,6 +23,7 @@ For str = "aabaa", the output should be true.
 For str = "abac", the output should be false.
 For str = "a", the output should be true.
 -}
+
 checkPalindrome :: [Char] -> Bool
 checkPalindrome str = str == reverse str
 
@@ -30,6 +34,7 @@ and return that product.
 For xs = [3, 6, -2, -5, 7, 3], the output
 should be 21 (produced from 7 and 3).
 -}
+
 adjacentElementsProduct :: [Int] -> Int
 adjacentElementsProduct xs =
     let tupleAcc acc x = (snd (head acc)*x, x) : acc
@@ -55,9 +60,9 @@ additional statues needed.
 For statues = [6, 2, 3, 8], the output should be 3.
 Ratiorg needs statues of sizes 4, 5 and 7.
 -}
+
 makeArrayConsecutive2 :: [Int] -> Int
 makeArrayConsecutive2 xs = length ([(minimum xs)..(maximum xs)]) - length xs
-
 
 {-
 Given a sequence of integers as an array,
@@ -85,3 +90,110 @@ isIncreasing [_] = True
 isIncreasing (a:b:cs)
     | a < b = isIncreasing (b:cs)
     | otherwise = False
+
+{-
+The CodeBots all decided to move to a new building and live together.
+The building is represented by a rectangular matrix of rooms.
+Each cell in the matrix contains an integer, the price of the room.
+Some rooms are free but haunted, so all the bots are afraid of them.
+That is why any room that is free or located in the same column
+is not considered suitable for the bots to live in.
+Help calculate the total price of all suitable rooms in the building.
+For matrix = [[0, 1, 1, 2],
+              [0, 5, 0, 0],
+              [2, 0, 3, 3]], the total is 9.
+-}
+
+matrixElementsSum :: [[Int]] -> Int
+matrixElementsSum matrix =
+    let sumRow row = sum $ takeWhile (>0) row
+    in  sum $ map sumRow $ transpose matrix
+
+{-
+Given an array of strings, return another array
+containing all of its longest strings.
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"],
+the output should be ["aba", "vcd", "aba"].
+-}
+
+allLongestStrings :: [[Char]] -> [[Char]]
+allLongestStrings xxs =
+    let maxLength = maximum $ map length xxs
+        filterMax xs = length xs == maxLength
+    in  filter filterMax xxs
+
+{-
+Given an array of strings, return another array
+containing all of its longest strings.
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"],
+the output should be ["aba", "vcd", "aba"].
+-}
+
+allLongestStrings :: [[Char]] -> [[Char]]
+allLongestStrings xxs =
+    let maxLength = maximum $ map length xxs
+        filterMax xs = length xs == maxLength
+    in  filter filterMax xxs
+
+{-
+Given two strings, find the number of common characters between them.
+For s1 = "aabcc" and s2 = "adcaa", the output should be 3.
+The strings have 3 common characters - 2 "a"s and 1 "c".
+-}
+
+commonCharacterCount :: [Char] -> [Char] -> Int
+commonCharacterCount s1 s2 =
+  length $ snd (foldl consume (s2, "") s1)
+
+consume :: ([Char], [Char]) -> Char -> ([Char], [Char])
+consume ss@(s2, s3) c
+    | elem c s2 = (delete c s2, s3 ++ [c])
+    | otherwise = ss
+
+{-
+Ticket numbers usually consist of an even number of digits.
+A ticket number is considered lucky if the sum of the first
+half of the digits is equal to the sum of the second half.
+Given a ticket number n, determine if it's lucky or not.
+For n = 1230, the output should be true;
+For n = 239017, the output should be false.
+-}
+
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+sumIntChars :: [Char] -> Int
+sumIntChars cs = sum $ map (\c -> read [c]) cs
+
+isLucky :: Int -> Bool
+isLucky n =
+    let (h1, h2)        = halve (show n)
+    in  sumIntChars h1 == sumIntChars h2
+
+{-
+Some people are standing in a row in a park.
+There are trees between them which cannot be moved.
+Your task is to rearrange the people by their heights
+in a non-descending order without moving the trees.
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output
+should be [-1, 150, 160, 170, -1, -1, 180, 190].
+-}
+
+reinsert :: ([Int], [Int]) -> Int -> ([Int], [Int])
+reinsert (ps,ys) x
+    | x == -1   = (ps,(x:ys))
+    | otherwise = ((tail ps),((head ps):ys))
+
+sortByHeight :: [Int] -> [Int]
+sortByHeight xs =
+    let ps = sort (filter (/=(-1)) xs)
+    in  reverse $ snd (foldl reinsert (ps,[]) xs)
+
+-- A similar solution by Ciunkos:
+zipC :: [Int] -> [Int] -> [Int]
+zipC a      _      = a
+zipC (a:as) (b:bs)
+    | a >= 0    = b : zipC as bs
+    | otherwise = a : zipC as (b:bs)
+sortByHeight a = zipC a (sort (filter (>= 0) a))
+
