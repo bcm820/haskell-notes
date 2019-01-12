f144e19423843b7b1e46975b1bda78f22d2c90e2
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2019-01-12 17:45:39 -0500

    update syntax to use where

--- codeFights.hs
+++ codeFights.hs
@@ -1,3 +1,4 @@
+import           Data.Char
 import           Data.List
 
 {-
@@ -10,12 +11,8 @@ For y = 1700, the output should be 17.
 -}
 
 centuryFromYear :: Int -> Int
-centuryFromYear y
-  | m == 0    = d
-  | otherwise = d + 1
-  where
-    m = mod y 100
-    d = div y 100
+centuryFromYear y = if m == 0 then d else d + 1
+  where m = mod y 100; d = div y 100
 
 {-
 Given the string, check if it is a palindrome.
@@ -37,17 +34,17 @@ should be 21 (produced from 7 and 3).
 
 adjacentElementsProduct :: [Int] -> Int
 adjacentElementsProduct xs =
-    let tupleAcc acc x = (snd (head acc)*x, x) : acc
-        initVal = [(0, head xs)]
-    in  maximum (init (map fst (foldl tupleAcc initVal (tail xs))))
+  maximum (init (map fst (foldl tupleAcc initVal (tail xs)))) where
+    tupleAcc acc x = (snd (head acc)*x, x) : acc
+    initVal = [(0, head xs)]
 
 -- Other solutions by nickie, ameiva, code_g5:
-adjacentElementsProduct xs = maximum $ zipWith (*) x (tail xs)
-adjacentElementsProduct xs = foldl1 max $ zipWith (*) xs (tail xs)
-adjacentElementsProduct = maximum . products
-    where products []       = []
-          products (x:[])   = []
-          products (x:y:xs) = x * y : products (y:xs)
+adjacentElementsProduct' xs = maximum $ zipWith (*) xs (tail xs)
+adjacentElementsProduct'' xs = foldl1 max $ zipWith (*) xs (tail xs)
+adjacentElementsProduct''' = maximum . products where
+  products []       = []
+  products (x:[])   = []
+  products (x:y:xs) = x * y : products (y:xs)
 
 {-
 Ratiorg got statues of different sizes as a present from CodeMaster,
@@ -62,7 +59,8 @@ Ratiorg needs statues of sizes 4, 5 and 7.
 -}
 
 makeArrayConsecutive2 :: [Int] -> Int
-makeArrayConsecutive2 xs = length ([(minimum xs)..(maximum xs)]) - length xs
+makeArrayConsecutive2 xs = length minToMax - length xs
+  where minToMax = [(minimum xs)..(maximum xs)]
 
 {-
 Given a sequence of integers as an array,
@@ -74,22 +72,22 @@ You can remove 3 from the array to get the strictly increasing sequence [1, 2].
 Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 -}
 
+isIncreasing :: [Int] -> Bool
+isIncreasing [_] = True
+isIncreasing (a:b:cs)
+  | a < b = isIncreasing (b:cs)
+  | otherwise = False
+
 almostIncreasingSequence :: [Int] -> Bool
 almostIncreasingSequence [] = True
 almostIncreasingSequence [_] = True
 almostIncreasingSequence [_,_] = True
 almostIncreasingSequence (a:b:c:ds)
-    | a < b && b < c = almostIncreasingSequence (b:c:ds)
-    | a < b && a < c = isIncreasing (a:c:ds)
-    | a > b && b < c = isIncreasing (b:c:ds)
-    | a < b && c <= a = isIncreasing (a:b:ds)
-    | otherwise = False
-
-isIncreasing :: [Int] -> Bool
-isIncreasing [_] = True
-isIncreasing (a:b:cs)
-    | a < b = isIncreasing (b:cs)
-    | otherwise = False
+  | a < b && b < c  = almostIncreasingSequence (b:c:ds)
+  | a < b && a < c  = isIncreasing (a:c:ds)
+  | a > b && b < c  = isIncreasing (b:c:ds)
+  | a < b && c <= a = isIncreasing (a:b:ds)
+  | otherwise = False
 
 {-
 The CodeBots all decided to move to a new building and live together.
@@ -105,9 +103,8 @@ For matrix = [[0, 1, 1, 2],
 -}
 
 matrixElementsSum :: [[Int]] -> Int
-matrixElementsSum matrix =
-    let sumRow row = sum $ takeWhile (>0) row
-    in  sum $ map sumRow $ transpose matrix
+matrixElementsSum matrix = sum $ map sumRow $ transpose matrix
+  where sumRow row = sum $ takeWhile (>0) row
 
 {-
 Given an array of strings, return another array
@@ -117,23 +114,9 @@ the output should be ["aba", "vcd", "aba"].
 -}
 
 allLongestStrings :: [[Char]] -> [[Char]]
-allLongestStrings xxs =
-    let maxLength = maximum $ map length xxs
-        filterMax xs = length xs == maxLength
-    in  filter filterMax xxs
-
-{-
-Given an array of strings, return another array
-containing all of its longest strings.
-For inputArray = ["aba", "aa", "ad", "vcd", "aba"],
-the output should be ["aba", "vcd", "aba"].
--}
-
-allLongestStrings :: [[Char]] -> [[Char]]
-allLongestStrings xxs =
-    let maxLength = maximum $ map length xxs
-        filterMax xs = length xs == maxLength
-    in  filter filterMax xxs
+allLongestStrings xxs = filter filterMax xxs where
+  filterMax xs = length xs == maxLength
+  maxLength = maximum $ map length xxs
 
 {-
 Given two strings, find the number of common characters between them.
@@ -142,11 +125,8 @@ The strings have 3 common characters - 2 "a"s and 1 "c".
 -}
 
 commonCharacterCount :: [Char] -> [Char] -> Int
-commonCharacterCount s1 s2 =
-  length $ snd (foldl consume (s2, "") s1)
-
-consume :: ([Char], [Char]) -> Char -> ([Char], [Char])
-consume ss@(s2, s3) c
+commonCharacterCount s1 s2 = length $ snd (foldl consume (s2, "") s1) where
+  consume ss@(s2, s3) c
     | elem c s2 = (delete c s2, s3 ++ [c])
     | otherwise = ss
 
@@ -159,16 +139,11 @@ For n = 1230, the output should be true;
 For n = 239017, the output should be false.
 -}
 
-halve :: [a] -> ([a], [a])
-halve xs = splitAt (length xs `div` 2) xs
-
-sumIntChars :: [Char] -> Int
-sumIntChars cs = sum $ map (\c -> read [c]) cs
-
 isLucky :: Int -> Bool
-isLucky n =
-    let (h1, h2)        = halve (show n)
-    in  sumIntChars h1 == sumIntChars h2
+isLucky n = sumIntChars h1 == sumIntChars h2 where
+  sumIntChars cs = sum $ map (\c -> read [c]) cs
+  (h1, h2) = splitInHalf (show n)
+  splitInHalf xs = splitAt (length xs `div` 2) xs
 
 {-
 Some people are standing in a row in a park.
@@ -180,20 +155,66 @@ should be [-1, 150, 160, 170, -1, -1, 180, 190].
 -}
 
 reinsert :: ([Int], [Int]) -> Int -> ([Int], [Int])
-reinsert (ps,ys) x
-    | x == -1   = (ps,(x:ys))
-    | otherwise = ((tail ps),((head ps):ys))
+reinsert ([], ys) x = ([],x:ys)
+reinsert (ps@(p:qs),ys) x
+  | x == -1   = (ps,(x:ys))
+  | otherwise = (qs,(p:ys))
 
 sortByHeight :: [Int] -> [Int]
 sortByHeight xs =
-    let ps = sort (filter (/=(-1)) xs)
-    in  reverse $ snd (foldl reinsert (ps,[]) xs)
+  let ps = sort (filter (/=(-1)) xs)
+  in  reverse $ snd (foldl reinsert (ps,[]) xs)
 
 -- A similar solution by Ciunkos:
-zipC :: [Int] -> [Int] -> [Int]
-zipC a      _      = a
-zipC (a:as) (b:bs)
+sortByHeight' a = zipC a (sort (filter (>= 0) a)) where
+  zipC (a:as) (b:bs)
     | a >= 0    = b : zipC as bs
     | otherwise = a : zipC as (b:bs)
-sortByHeight a = zipC a (sort (filter (>= 0) a))
 
+{-
+Write a function that reverses characters in (possibly nested) parentheses
+in the input string, which will always be well-formed with matching ()s.
+For inputString = "foo(bar)baz", the output should be "foorabbaz";
+For inputString = "foo(bar)baz(blim)", the output should be "foorabbazmilb".
+-}
+
+-- ATTEMPT #1 (only works for nested parens :/)
+
+splitAtParens :: [Char] -> ([Char], [Char], [Char])
+splitAtParens s = (left, inner, right) where
+  sliceOnce p xs = (takeWhile p xs, dropWhile p xs)
+  isNotParen c    = c /= '(' && c /= ')'
+  (left, rest)    = sliceOnce isNotParen s
+  (iRight, iMid)  = sliceOnce isNotParen (reverse rest)
+  (mid, right)    = (reverse iMid, reverse iRight)
+  inner           = filter isNotParen mid
+
+reverseInParentheses :: [Char] -> [Char]
+reverseInParentheses s
+  | hasNoParen s  = s
+  | otherwise     = evalParens (splitAtParens s) where
+    hasNoParen s = notElem '(' s && notElem ')' s
+    evalParens (left, inner, right)
+      | hasNoParen inner  = left ++ reverse inner ++ right
+      | otherwise         = left ++ innerTrio ++ right where
+        innerTrio = reverse (evalParens (splitAtParens inner))
+
+-- ATTEMPT #2 (works!)
+
+findParens :: [Char] -> [(Int, Int)]
+findParens s = foldl findRanges [] (zip s [0..length s - 1]) where
+  findRanges xs (c, idx)
+    | c == '('  = ((idx, -1):xs)
+    | c == ')'  = tail xs ++ [(fst (head xs), idx)]
+    | otherwise = xs
+
+splitTwice :: Int -> Int -> [Char] -> ([Char], [Char], [Char])
+splitTwice a b s = (x, y1, y2) where
+  (x, y)   = splitAt a s
+  (y1, y2) = splitAt (b - length x + 1) y
+
+reverseInParentheses' :: [Char] -> [Char]
+reverseInParentheses' s =
+  filter isAlpha $ foldl revSubList s (findParens s) where
+  revSubList s (a, b)   = left ++ reverse inner ++ right where
+    (left, inner, right)  = splitTwice a b s
